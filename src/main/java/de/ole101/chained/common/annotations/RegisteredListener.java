68acf4d4bb1adb4420e1331305e600f2e69fe1fb
Felix <29801805+ImGxrke@users.noreply.github.com>
2024-08-27 21:25:26 +0200

    fix: add missing @IndexAnnotated

--- src/main/java/de/ole101/chained/common/annotations/RegisteredListener.java
+++ src/main/java/de/ole101/chained/common/annotations/RegisteredListener.java
@@ -1,11 +1,15 @@
 package de.ole101.chained.common.annotations;
 
+import org.atteo.classindex.IndexAnnotated;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@IndexAnnotated
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RegisteredListener {
+
 }
