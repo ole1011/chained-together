df3e3b997c4ab9ac007e0f1f0a3fdf4435735db1
ole101 <oleh69250@gmail.com>
2024-08-29 18:17:34 +0200

    feat: add new colors

--- src/main/java/de/ole101/chained/common/Colors.java
+++ src/main/java/de/ole101/chained/common/Colors.java
@@ -4,11 +4,19 @@ import net.kyori.adventure.text.format.TextColor;
 
 public class Colors {
 
-    public static final TextColor BLUE = TextColor.fromHexString("#0095FF");
-    public static final TextColor RED = TextColor.fromHexString("#FF3B3B");
-    public static final TextColor SKY_BLUE = TextColor.fromHexString("#B9F2FF");
-    public static final TextColor GREEN = TextColor.fromHexString("#3FED56");
-    public static final TextColor WHITE = TextColor.fromHexString("#fcfcfc");
+    public static final TextColor GOLD = TextColor.fromHexString("#FFB300");
+    public static final TextColor LIGHT_GRAY = TextColor.fromHexString("#E0E0E0");
     public static final TextColor GRAY = TextColor.fromHexString("#B0B0B0");
+    public static final TextColor DARK_GRAY = TextColor.fromHexString("#615F5F");
+    public static final TextColor BLUE = TextColor.fromHexString("#0095FF");
+    public static final TextColor GREEN = TextColor.fromHexString("#3FED56");
+    public static final TextColor RED = TextColor.fromHexString("#FF3B3B");
+    public static final TextColor PURPLE = TextColor.fromHexString("#B300FF");
+    public static final TextColor DARK_PURPLE = TextColor.fromHexString("#734bbf");
     public static final TextColor YELLOW = TextColor.fromHexString("#fcfc54");
+    public static final TextColor WHITE = TextColor.fromHexString("#fcfcfc");
+    public static final TextColor PINK = TextColor.fromHexString("#FA5DA5");
+    public static final TextColor LIGHT_PINK = TextColor.fromHexString("#EA6EF9");
+    public static final TextColor BROWN = TextColor.fromHexString("#AA5500");
+    public static final TextColor SKY_BLUE = TextColor.fromHexString("#B9F2FF");
 }
