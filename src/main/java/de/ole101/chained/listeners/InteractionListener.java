25b5bc0db34131d3365da12e49fcb7f47923c3f2
ole101 <oleh69250@gmail.com>
2024-08-30 18:06:15 +0200

    feat: add rope pulling

--- src/main/java/de/ole101/chained/listeners/InteractionListener.java
+++ src/main/java/de/ole101/chained/listeners/InteractionListener.java
@@ -3,10 +3,13 @@ package de.ole101.chained.listeners;
 import com.google.inject.Inject;
 import de.ole101.chained.common.annotations.RegisteredListener;
 import de.ole101.chained.services.ChainService;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+import org.bukkit.util.Vector;
 
 import static org.bukkit.inventory.EquipmentSlot.OFF_HAND;
 
@@ -42,5 +45,40 @@ public class InteractionListener implements Listener {
         }
 
         this.chainService.requestChaining(player, target);
+    }
+
+    @EventHandler
+    public void onSwapItems(PlayerSwapHandItemsEvent event) {
+        Player player = event.getPlayer();
+        if (this.chainService.getActiveChains().stream().noneMatch(chain -> chain.getPlayer().equals(player) || chain.getTarget().equals(player))) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+        Player target = this.chainService.getActiveChains().stream()
+                .filter(chain -> chain.getPlayer().equals(player) || chain.getTarget().equals(player))
+                .map(chain -> chain.getPlayer().equals(player) ? chain.getTarget() : chain.getPlayer())
+                .findFirst()
+                .orElseThrow();
+
+        Location playerLocation = player.getLocation();
+        Location targetLocation = target.getLocation();
+        Vector playerLocationVector = playerLocation.toVector();
+        Vector targetLocationVector = targetLocation.toVector();
+
+        Vector velocity = playerLocationVector.subtract(targetLocationVector).normalize().multiply(0.3);
+
+        double playerY = player.getY();
+        double targetY = target.getY();
+        if (playerY != targetY) {
+            velocity.setY(playerY > targetY ? 0.3 : -0.3);
+        }
+
+        if (playerLocation.distance(targetLocation) < 0.5) {
+            velocity = new Vector();
+        }
+
+        target.setVelocity(velocity);
     }
 }
