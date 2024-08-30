55b3fbdd25e4a97e94643d0096edacd3d90bd6d4
ole101 <oleh69250@gmail.com>
2024-08-30 18:16:45 +0200

    feat: add rope pulling hint

--- src/main/java/de/ole101/chained/common/runnables/ChainRunnable.java
+++ src/main/java/de/ole101/chained/common/runnables/ChainRunnable.java
@@ -3,14 +3,23 @@ package de.ole101.chained.common.runnables;
 import de.ole101.chained.common.models.Chain;
 import de.ole101.chained.services.ChainService;
 import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Slime;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
+import static de.ole101.chained.common.Colors.LIGHT_GRAY;
+import static de.ole101.chained.common.Colors.YELLOW;
+import static net.kyori.adventure.text.Component.keybind;
+import static net.kyori.adventure.text.Component.text;
+
 @RequiredArgsConstructor
 public class ChainRunnable extends BukkitRunnable {
 
+    private static final Component ACTION_BAR_COMPONENT = text("Tipp: Drücke", LIGHT_GRAY).appendSpace()
+            .append(keybind("key.swapOffhand", YELLOW)).appendSpace()
+            .append(text("um deinen Partner zu dir zu ziehen.", LIGHT_GRAY));
     private final ChainService chainService;
     private final Chain chain;
 
@@ -25,6 +34,9 @@ public class ChainRunnable extends BukkitRunnable {
             cancel();
             return;
         }
+
+        player.sendActionBar(ACTION_BAR_COMPONENT);
+        target.sendActionBar(ACTION_BAR_COMPONENT);
 
         double distance = player.getLocation().distance(target.getLocation());
         double chainLength = this.chain.getDifficulty().getChainLength();
